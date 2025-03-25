c7db9ebaa6df487454bd51eb3ef1e8577bfc1ac9
ulloa011205 <lesterulloa2005@gmail.com>
2025-03-25 16:02:16 -0600

    sql

---
+++ clases.sql
@@ -0,0 +1,23 @@
+INSERT INTO Products(ProductID, ProductName, SupplierID, CategoryID, Unit, Price)
+VALUES 
+(88, 'Conos', 35, 8, '2 bolsas', 10.00),
+(89, 'Botas Esqui', 13, 10, '4 cajas', 50.00),
+(90, 'Bicicletas', 4, 1, '5 cajas', 700.00),
+(91, 'Bicicleta electrica plegable', 23, 3, '5 cajas', 900.99),
+(92, 'Ropa ciclismo', 12, 5, '2 cajas', 5.00),
+(93, 'Ropa padel', 6, 7, '2 cajas', 10.00),
+(94, 'Pies de gato', NULL, 9, '3 cajas', 25.00),
+(95, 'Cinta de correr', 12, 11, '6 cajas', 2.00),
+(96, 'Bicicleta de montaña', 23, 1, '5 cajas', 899.00),
+(97, 'Pala de padel', 12, 3, '2 cajas', 15.00),
+(98, 'Bañador Mujer', 32, 5, '2 cajas', 2.50),
+(99, 'Bañador Hombre', 14, 7, '3 cajas', 2.50),
+(100, 'Botas de montaña', 14, 9, '4 cajas', 15.00),
+(101, 'Casco esqui', 15, 1, '3 cajas', 25.00),
+(102, 'Mochila deporte', 12, 3, '1 caja', 30.50),
+(103, 'Pelota de tennis', 12, 5, '3 cajas', 1.25),
+(104, 'Reloj con GPS', 7, 7, '2 cajas', 80.00),
+(105, 'Bastones esqui', 13, 9, '5 cajas', 25.00),
+(106, 'Casco ciclismo', 12, 2, '3 cajas', 25.00),
+(107, 'Bicicleta carretera', 14, 4, '4 cajas', 1000.00);
+SELECT* FROM Products
